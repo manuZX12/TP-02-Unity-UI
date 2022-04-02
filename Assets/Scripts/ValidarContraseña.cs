f2ac9849a0fa0add14f2c5186673cf31a88bc5f3
Manuel Magni <61920152+manuZX12@users.noreply.github.com>
2022-04-02 11:04:30 -0300

    C# for Unity 06 done

--- Assets/Scripts/ValidarContraseña.cs
+++ Assets/Scripts/ValidarContraseña.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ValidarContraseña : MonoBehaviour
 {
+    string contracorrecta;
+    string contrausuario;
+    public Text ingresousuario;
     // Start is called before the first frame update
     void Start()
     {
-        
+        contracorrecta = "12345";
     }
 
     // Update is called once per frame
@@ -17,7 +20,15 @@ public class ValidarContraseña : MonoBehaviour
     }
     public void validarContrsenia()
     {
-
+        contrausuario = ingresousuario.text;
+        if (contrausuario == contracorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña correcta");
+        }
     }
     
 }
