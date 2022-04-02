0eb18e5d778347d71dc649731017909a101dfa67
Manuel Magni <61920152+manuZX12@users.noreply.github.com>
2022-04-02 10:17:22 -0300

    C# for Unity 02 done

--- Assets/Scripts/Mayoriadeedad.cs
+++ Assets/Scripts/Mayoriadeedad.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Mayoriadeedad : MonoBehaviour
 {
+    public int edadusuario;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (edadusuario >= 18)
+        {
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
